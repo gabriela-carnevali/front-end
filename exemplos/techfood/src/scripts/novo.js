d6847c573f59a8ccf96dc1012585dcac20577c2a
gabriela-carnevali <gabriela.c.lima10@aluno.senai.br>
2026-03-04 16:02:42 -0300

    Início do estudo de DOM

---
+++ exemplos/techfood/src/scripts/novo.js
@@ -0,0 +1,38 @@
+const tituloNhoque = document.querySelector('#card-nhoque h3')
+
+const botoesCompra = document.querySelectorAll('.bt-pedido')
+
+const terceiroCard = document.querySelector('.card:nth-child(3)')
+
+const nomeCompleto = document.querySelector('#nome')
+
+console.log("1. Mostrando o Título NHOQUE (pelo ID)", tituloNhoque)
+
+console.log("2. Quantidade de botões de pedido:", botoesCompra.length)
+
+console.log("3. A terceira posição da class card: ", terceiroCard)
+
+const imgLasanha = document.querySelector('img[alt="Lasanha à bolonhesa com 4 queijos]')
+
+if (tituloNhoque) {
+    console.log("Título CAPTURADO: ", tituloNhoque.innerText)
+}
+
+const saudacao = document.querySelector('#boas-vindas')
+
+const agora = new Date()
+
+const hora = agora.getHours()
+
+saudacao.textContent = hora < 18 ? "Bem-vindo! Bom almoço!" : "Bem-vindo! Boa janta!"
+
+nomeCompleto.innerHTML = "<strong>Gabriela</strong> Carnevali Gonçalves <em>Lima</em>"
+
+
+
+
+
+const foto = document.querySelector('#foto-destaque')
+
+const caminhoAtual = foto.getAttribute('src')
+
